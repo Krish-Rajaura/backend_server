53d06dbb823f7cf435859bacdc4446e9aa2f9eae
krish <krishrajaura96@gmail.com>
2025-12-27 14:13:43 +0530

    creating user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,67 @@
 import mongoose, { Schema } from 'mongoose'
+import bcrypt from "bcrypt"
 
 const userSchema= new Schema({
     username:{
+        type:String,
         required:true,
         unique:true,
-        
-
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true
+    },
+    fullName:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    coverImage:{
+        type:String,
+    },
+    watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ],
+    password:{
+        type:String,
+        required:[true,"Password is required"]
+    },
+    refreshTokens:{
+        type:String
     }
+},
+
+{
+    timestamps:true
+}
+
+)
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next();
+    
+    this.password= await bcrypt.hash(this.password,10)
+    next();
+    
 })
+
+userSchema.methods.IsPasswordCorrect=async function(password){
+
+    return await bcrypt.compare(password,this.password)
+}
+
+export const User=mongoose.model("User",userSchema)
